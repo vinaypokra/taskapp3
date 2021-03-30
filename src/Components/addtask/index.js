e2d79eb93f25d61e7941e4035bcd6ff7f6d6210c
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-03-31 03:58:48 +0530

    calender update

--- src/Components/addtask/index.js
+++ src/Components/addtask/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Calendar from "./Calendar/calendar";
 const Addtask = () => {
   return (
     <>
       <h1>Add new task upload here</h1>
+      <Calendar />
     </>
   );
 };
