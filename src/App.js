49537095ea01f9234b34669d71671317f8e21042
Bhagwansahay1 <bhagwansahay12110@gmail.com>
2021-03-31 05:05:13 +0530

    signup updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import TaskPage from "./Components/addtask/";
 import Dashboard from "./Components/dashboard";
 import { BrowserRouter as Routers, Route, Link } from "react-router-dom";
 import Navigation from "./Components/Drawer";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
+import Amplify from "aws-amplify";
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <>
       <Routers>
         <Navigation>
+          <AmplifySignOut />
           <Route path="/taskpage">
             <TaskPage />
           </Route>
@@ -18,6 +34,8 @@ function App() {
         </Navigation>
       </Routers>
     </>
+  ) : (
+    <AmplifyAuthenticator />
   );
 }
 
