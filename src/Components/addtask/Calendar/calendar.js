0b1ebae7043ad95694e1c3726c43753170949d17
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-03-31 01:50:01 +0530

    Initialize project using Create React App

---
+++ src/Components/addtask/Calendar/calendar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "./Cal.css";
+import moment from "moment";
+import { Grid } from "@material-ui/core";
+
+export default function Cal() {
+  const [dateState, setDateState] = useState(new Date());
+  const changeDate = (e) => {
+    setDateState(e);
+  };
+
+  return (
+    <>
+      <Calendar
+        value={dateState}
+        onChange={changeDate}
+        onClickDay={props.handleClickOpen}
+      />
+      <Grid item>
+        <p style={{ margin: "30px 100px" }}>
+          Current selected date is{" "}
+          <b>{moment(dateState).format("MMMM Do YYYY")}</b>
+        </p>
+      </Grid>
+    </>
+  );
+}
