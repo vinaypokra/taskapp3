fc492ccfc9692e874dcb727a01ac0e264d7fc49f
Bhagwansahay1 <bhagwansahay12110@gmail.com>
2021-04-01 02:39:29 +0530

    profile updated

--- src/Components/dashboard/index.js
+++ src/Components/dashboard/index.js
@@ -1,11 +1,89 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import MuiPhoneNumber from "material-ui-phone-number";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
-const DashBoard = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Dashboard() {
+  const classes = useStyles();
+  function handleOnChange(value) {
+    this.setState({
+      phone: value,
+    });
+  }
+
   return (
-    <>
-      <h1>DashBoard task upload here</h1>
-    </>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="fname"
+              name="firstName"
+              variant="outlined"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="LastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="lname"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="date"
+              label="Birthday"
+              type="date"
+              defaultValue="2017-05-24"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <MuiPhoneNumber
+              name="phone"
+              label="Phone Number"
+              data-cy="user-phone"
+              defaultCountry={"in"}
+            />
+          </Grid>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.button}
+            startIcon={<SaveIcon />}
+          >
+            Save
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
   );
-};
-
-export default DashBoard;
+}
