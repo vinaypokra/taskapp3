6485f049213797102e82dc35dd880b4a42efbeb7
Bhagwansahay1 <bhagwansahay12110@gmail.com>
2021-04-01 03:41:54 +0530

    basic updated

--- src/Components/dashboard/index.js
+++ src/Components/dashboard/index.js
@@ -11,10 +11,10 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+  form: {
+    width: "100%",
+    height: "100vh", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
   },
 }));
 
@@ -28,62 +28,64 @@ export default function Dashboard() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              autoComplete="fname"
-              name="firstName"
-              variant="outlined"
-              required
-              fullWidth
-              id="firstName"
-              label="First Name"
-              autoFocus
-            />
+      <form className={classes.form} noValidate>
+        <Grid container spacing={3}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="LastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="date"
+                label="Birthday"
+                type="date"
+                defaultValue="2017-05-24"
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <MuiPhoneNumber
+                name="phone"
+                label="Phone Number"
+                data-cy="user-phone"
+                defaultCountry={"in"}
+              />
+            </Grid>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              className={classes.button}
+              startIcon={<SaveIcon />}
+            >
+              Save
+            </Button>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="LastName"
-              label="Last Name"
-              name="lastName"
-              autoComplete="lname"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="date"
-              label="Birthday"
-              type="date"
-              defaultValue="2017-05-24"
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <MuiPhoneNumber
-              name="phone"
-              label="Phone Number"
-              data-cy="user-phone"
-              defaultCountry={"in"}
-            />
-          </Grid>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            className={classes.button}
-            startIcon={<SaveIcon />}
-          >
-            Save
-          </Button>
         </Grid>
-      </Grid>
+      </form>
     </div>
   );
 }
