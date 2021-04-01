596bf5eed8063c801091a2f08e59e55dc486c1ee
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-04-02 04:24:04 +0530

    commited by vinay

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers/";
 import thunk from "redux-thunk";
 const store = createStore(reducers, applyMiddleware(thunk));
