953afa660bd2e997fab37955ccf6ad51690072aa
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-04-04 02:47:01 +0530

    commited status

--- src/Components/Profile/Userprofileform.js
+++ src/Components/Profile/Userprofileform.js
@@ -83,7 +83,7 @@ const Userprofileform = (props) => {
       Resume: userdata.Resume,
       IDCard: userdata.ID,
       Email: sessionStorage.getItem("userName"),
-      Status: "pending",
+      Status: "Pending...",
     });
     props.userProfileInfo(sessionStorage.getItem("userName"));
   };
