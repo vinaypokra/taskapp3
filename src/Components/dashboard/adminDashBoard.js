799ab9fbc785f5b534074e2820f671cf32a18794
asharma991 <asharma991@outlook.com>
2021-04-06 03:29:53 +0530

    minor

--- src/Components/dashboard/adminDashBoard.js
+++ src/Components/dashboard/adminDashBoard.js
@@ -35,7 +35,7 @@ const Dashboard = () => {
       <h1 style={{ color: "black" }}>Dashboard Page</h1>
       <Grid item container direction="row" spacing={5}>
         <Grid item xs={6}>
-          <CardActionArea onClick={() => window.open("/allusers", "_blank")}>
+          <CardActionArea onClick={() => history.push("/allusers")}>
             <Card
               className={classes.card}
               style={{
@@ -62,7 +62,7 @@ const Dashboard = () => {
           </CardActionArea>
         </Grid>
         <Grid item xs={6}>
-          <CardActionArea onClick={() => window.open("/alltasks", "_blank")}>
+          <CardActionArea onClick={() => history.push("/alltasks")}>
             <Card
               className={classes.card}
               style={{
